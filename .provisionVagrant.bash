aeced0ed5b846296c9aab35e722b3fcd7c725866
Matt Gowland <u1038652@umail.usq.edu.au>
2016-08-07 00:24:18 +1000

    Add base boilerplate to template

--- .provisionVagrant.bash
+++ .provisionVagrant.bash
@@ -8,8 +8,9 @@ echo "Creating .env..."
 cp ../.env.scotchbox .env
 php artisan key:generate > /dev/null
 
-#echo "Installing dependencies with npm..."
-#sudo chown -R $(whoami) $(npm config get prefix)/{lib/node_modules,bin,share} > /dev/null
-#npm upgrade -g npm
-#npm install
+echo "Installing dependencies with npm..."
+sudo chown -R $(whoami) $(npm config get prefix)/{lib/node_modules,bin,share} > /dev/null
+npm install bootstrap-sass
+npm install vue
+sudo npm install -g browserify
 
